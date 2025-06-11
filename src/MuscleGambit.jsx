810a4ac456ded1c191d3d28d2ed1482fcb198a77
Karteks33 <Superkapi130@gmail.com>
2025-06-11 20:43:05 +0200

    Update MuscleGambit.jsx

--- src/MuscleGambit.jsx
+++ src/MuscleGambit.jsx
@@ -40,7 +40,7 @@ export default function MuscleGambit() {
           <img 
             src="/pogba.jpg" 
             alt="30zł" 
-            className="w-32 h-auto cursor-pointer hover:opacity-80 transition-opacity duration-300"
+            className="w-15 h-auto cursor-pointer hover:opacity-80 transition-opacity duration-300"
             onClick={() => handleRedirect("https://buy.stripe.com/test_hetmanskiSznyt")}
           />
         </section>
